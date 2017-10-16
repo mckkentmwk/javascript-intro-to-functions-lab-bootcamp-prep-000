a044426163cc2ba5e3b83454cedceacb7422dc1a
Kent Yucra <mckkentmwk@gmail.com>
2017-10-16 19:09:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
+
+function sayHiToGrandma(string){
+  if(string == "hello"){
+    return "I can't hear you!"
+  }else if(string == "HELLO"){
+    return "YES INDEED!"
+  }else if(string == "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
