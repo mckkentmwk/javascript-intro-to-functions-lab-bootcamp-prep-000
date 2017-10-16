3737807240961c98ffdebd21f8928d1041919467
Kent Yucra <mckkentmwk@gmail.com>
2017-10-16 19:15:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function whisper(string){
   return string.toLowerCase()
 }
 
+function logShout(string){
+  string = string.toUpperCase()
+  console.log(string)
+}
+
 function logWhisper(string){
   string = string.toLowerCase()
   console.log(string)
