8a7b1077673ac845fb062d172e5063a113aedeb7
Kent Yucra <mckkentmwk@gmail.com>
2017-10-16 19:12:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function whisper(string){
   return string.toLowerCase()
 }
 
+function logWhisper(string){
+  console.log(string)
+}
+
 function sayHiToGrandma(string){
   if(string == "hello"){
     return "I can't hear you!"
